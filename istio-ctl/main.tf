016f3675dce3da1eb157c48830701273d536ee68
pbak_c <patryk.bak@bluesoft.com>
2022-10-31 17:20:51 +0100

    add experimental istio-ctl tf module

---
+++ istio-ctl/main.tf
@@ -0,0 +1,29 @@
+resource "kubernetes_namespace" "istio_namespace" {
+  metadata {
+    annotations = {}
+
+    labels = {}
+
+    name = var.namespace
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metadata[0].labels,
+      metadata[0].annotations
+    ]
+  }
+}
+
+resource "null_resource" "istio" {
+  depends_on = [kubernetes_namespace.istio_namespace]
+
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers = {
+    istio_system_namespace = kubernetes_namespace.istio_namespace.uid
+  }
+
+  provisioner "local-exec" {
+    command = "./resources/istioctl install --set profile=ambient"
+  }
+}
---
+++ istio-ctl/variables.tf
@@ -0,0 +1,5 @@
+variable "namespace" {
+  type        = string
+  description = "Istio installation namespace"
+  default     = "istio-system"
+}
